88395d6cbcb8989512da4274b9ac506a99ece6dd
Nevo Levy <nevo.levi@mail.huji.ac.il>
2024-11-02 17:07:29 +0200

    nothing important

--- src/WindowObj.h
+++ src/WindowObj.h
@@ -3,10 +3,11 @@
 
 class WindowObj {
 /**
- * @brief This class is a parent class for all of the objects that need to be generated on the screen (buttons, textboxes and so on) new
+ * @brief This class is a parent class for all of the objects that need to be generated on the screen (buttons, textboxes and so on)
  *
  */
 private:
+
 
 public:
 
